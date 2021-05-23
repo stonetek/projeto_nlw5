7068979598369757f80d03b9cb803afdd164fd4c
Pedro Silva <pelkorstone@gmail.com>
2021-05-23 08:19:04 -0300

    Classe Message criada e inserida na rota, servico e entidades

---
+++ src/Services/MessagesService.ts
@@ -0,0 +1,27 @@
+import { getCustomRepository } from 'typeorm';
+import { MessagesRepository } from '../repositories/MessagesRepository';
+
+interface iMessageCreate {
+  admin_id?: string;
+  text: string;
+  user_id: string;
+}
+
+class MessagesService {
+  async create({ admin_id, text, user_id }: iMessageCreate) {
+    const messagesRepository = getCustomRepository(MessagesRepository);
+
+    const message = messagesRepository.create({
+      admin_id,
+      text,
+      user_id
+    });
+
+    await messagesRepository.save(message);
+
+    return message;
+  }
+
+}
+
+export { MessagesService };
